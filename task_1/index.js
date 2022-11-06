586286a89a99d66966cb97bfee88ae8de4dd6faf
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-11-06 23:59:52 +0100

    [task_1]: task is done

--- task_1/index.js
+++ task_1/index.js
@@ -1,45 +1,45 @@
 function makeDeepCopy(obj) {
   //check if argument is object
   if (typeof obj !== 'object' || obj === null) {
-    throw new Error();
+    throw new Error()
   }
-  return recursionClone(obj);
+  return recursionClone(obj)
 }
 
  //function to recursevly copy an object
 function recursionClone(obj) {
-  let value;
+  let value
 
   if (typeof obj !== 'object' || obj === null) {
     // Return the value if obj is not an object
-    return obj;
+    return obj
   }
   //Check if property is Set. Convert it into Array.
   //Make deep clone and return new Set
   if (obj instanceof Set) {
-    const array = Array.from(obj);
-    const newSet = new Set(recursionClone(array));
-    return newSet;
+    const array = Array.from(obj)
+    const newSet = new Set(recursionClone(array))
+    return newSet
   }
   //Check if property is Map.Convert it into Object.
   //Make deep clone and return new Map
   if (obj instanceof Map) {
-    const objFromMap = Object.fromEntries(obj);
-    const newMap = new Map(Object.entries(recursionClone(objFromMap)));
-    return newMap;
+    const objFromMap = Object.fromEntries(obj)
+    const newMap = new Map(Object.entries(recursionClone(objFromMap)))
+    return newMap
   }
   // Create an array or object to hold the values
-  const newObj = Array.isArray(obj) ? [] : {};
+  const newObj = Array.isArray(obj) ? [] : {}
 
   //use Reflect.ownKeys() static method to iterate over
   //regular property names and Symbol property names
   for (let key of Reflect.ownKeys(obj)) {
-    value = obj[key];
+    value = obj[key]
     // Recursively (deep) copy for nested objects
-    newObj[key] = recursionClone(value);
+    newObj[key] = recursionClone(value)
   }
 
-  return newObj;
+  return newObj
 }
 
 //The only peculiarity of Reflect.ownKeys() method is that  method
@@ -50,47 +50,47 @@ function recursionClone(obj) {
 //function to recursevly copy an object (only enumarable property names and Symbols)
 //Please, check if makeDeepCopy() function is not commented (on line 1)
 // function recursionClone(obj) {
-//   let value;
+//   let value
 
 //   if (typeof obj !== 'object' || obj === null) {
 //     // Return the value if obj is not an object
-//     return obj;
+//     return obj
 //   }
 
 //   //Check if property is Set. Convert it into Array.
 //   //Make deep clone and return new Set
 //   if (obj instanceof Set) {
-//     const array = Array.from(obj);
-//     const newSet = new Set(recursionClone(array));
-//     return newSet;
+//     const array = Array.from(obj)
+//     const newSet = new Set(recursionClone(array))
+//     return newSet
 //   }
 
 //   //Check if property is Map.Convert it into Object.
 //   //Make deep clone and return new Map
 //   if (obj instanceof Map) {
-//     const objFromMap = Object.fromEntries(obj);
-//     const newMap = new Map(Object.entries(recursionClone(objFromMap)));
-//     return newMap;
+//     const objFromMap = Object.fromEntries(obj)
+//     const newMap = new Map(Object.entries(recursionClone(objFromMap)))
+//     return newMap
 //   }
 
 //   // Create an array or object to hold the values
-//   const newObj = Array.isArray(obj) ? [] : {};
+//   const newObj = Array.isArray(obj) ? [] : {}
 
 //   //use Reflect.ownKeys() static method to iterate over
 //   //regular property names and Symbol property names
 //   for (let key in obj) {
-//     value = obj[key];
+//     value = obj[key]
 //     // Recursively (deep) copy for nested objects
-//     newObj[key] = recursionClone(value);
+//     newObj[key] = recursionClone(value)
 //   }
 
 //   //also cloning a Symbol property names
 //   for (let key of Object.getOwnPropertySymbols(obj)) {
-//     value = obj[key];
-//     newObj[key] = recursionClone(value);
+//     value = obj[key]
+//     newObj[key] = recursionClone(value)
 //   }
 
-//   return newObj;
+//   return newObj
 // }
 
 // //I didn't find any differences in output between this two functions.
