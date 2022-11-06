2ca67e98e2c36a931d0d9f22ef70485003702e86
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-11-06 15:58:36 +0100

    Added algorithm to make deep copy of Symbol property names with Reflect.ownKeys()

--- task_1/index.js
+++ task_1/index.js
@@ -9,19 +9,43 @@ function makeDeepCopy(obj) {
   // Create an array or object to hold the values
   let newObj = Array.isArray(obj) ? [] : {}
 
-  for (let key in obj) {
+  for (let key of Reflect.ownKeys(obj)) {
 
     value = obj[key]
     // Recursively (deep) copy for nested objects
     newObj[key] = makeDeepCopy(value)
   }
 
-  //Algorithm that recursevly copying Symbol property names
-  for(let key of Object.getOwnPropertySymbols(obj)) {
-    value = obj[key]
+  // //Algorithm that recursevly copying Symbol property names
+  // for(let key of Object.getOwnPropertySymbols(obj)) {
+  //   value = obj[key]
 
-    newObj[key] = makeDeepCopy(value)
-  }
+  //   newObj[key] = makeDeepCopy(value)
+  // }
 
   return newObj
 }
+
+
+const obj = {
+  a: 1,
+  b: 2,
+  c: 4,
+  [Symbol('eee')]: 555,
+  l: {
+    ad:34,
+    [Symbol('kkk')] : {
+      kk: 1,
+      ff: 2,
+    },
+    4: 'aaa',
+    1: true
+  },
+  lasd: undefined,
+  trust: true,
+  cast: null
+}
+
+const newObj = makeDeepCopy(obj)
+console.log(obj)
+console.log(newObj)
