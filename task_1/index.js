83dcf408e8b5231ceb0e1931bcb6615732081585
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-11-06 15:32:37 +0100

    Added some comments

--- task_1/index.js
+++ task_1/index.js
@@ -1,17 +1,20 @@
+//Task is not finished yet! Need to add operations with Symbols
+
 function makeDeepCopy(obj) {
   let value
   
   if (typeof obj !== "object" || obj === null) {
     // Return the value if obj is not an object
     return obj 
-  }
+  } 
 
   // Create an array or object to hold the values
   let newObj = Array.isArray(obj) ? [] : {}
 
   for (let key in obj) {
-    value = obj[key]
+    console.log(typeof key)
 
+    value = obj[key]
     // Recursively (deep) copy for nested objects
     newObj[key] = makeDeepCopy(value)
   }
