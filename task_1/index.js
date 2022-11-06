fb08345f9309c16cfac26b5d21d939ace36541db
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-11-06 16:19:31 +0100

    Added argument validation

--- task_1/index.js
+++ task_1/index.js
@@ -1,22 +1,34 @@
 function makeDeepCopy(obj) {
-  let value
-  
+
+  //check if argument is object
   if (typeof obj !== "object" || obj === null) {
-    // Return the value if obj is not an object
-    return obj 
-  } 
+    throw new Error()
 
-  // Create an array or object to hold the values
-  let newObj = Array.isArray(obj) ? [] : {}
-  
-  //use Reflect.ownKeys() static method to iterate over
-  //regular property names and Symbol property names
-  for (let key of Reflect.ownKeys(obj)) {
+  }
+  //function to recursevly copy an object
+  function recursionClone(obj) {
+    let value
+    
+    if (typeof obj !== "object" || obj === null) {
+      // Return the value if obj is not an object
+      return obj 
+    } 
 
-    value = obj[key]
-    // Recursively (deep) copy for nested objects
-    newObj[key] = makeDeepCopy(value)
+    // Create an array or object to hold the values
+    let newObj = Array.isArray(obj) ? [] : {}
+    
+    //use Reflect.ownKeys() static method to iterate over
+    //regular property names and Symbol property names
+    for (let key of Reflect.ownKeys(obj)) {
+
+      value = obj[key]
+      // Recursively (deep) copy for nested objects
+      newObj[key] = recursionClone(value)
+    }
+
+    return newObj
   }
 
-  return newObj
+  return recursionClone(obj)
+
 }
