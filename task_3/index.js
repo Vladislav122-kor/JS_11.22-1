29c86b766199fa68685f4509146f7260f91551c1
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-11-06 22:53:12 +0100

    [task_3]: task is done

--- task_3/index.js
+++ task_3/index.js
@@ -1,31 +1,29 @@
 function createIterable(from, to) {
-  
   //validation
-  if( to <= from || !Number.isFinite(to) || !Number.isFinite(from)) {
-    throw new Error()
+  if (to <= from || !Number.isFinite(to) || !Number.isFinite(from)) {
+    throw new Error();
   }
 
   //creating an iterable object
-  let iterObj = {
+  const iterObj = {
     from,
     to,
     //adding custom [Symbol.iterator]
-    [Symbol.iterator] () {
-      this.current = this.from
-      return this
+    [Symbol.iterator]() {
+      this.current = this.from;
+      return this;
     },
-
     //next() is calling on every iteration of for...of loop
     next() {
-      if(this.current <= this.to) {
+      if (this.current <= this.to) {
         //next iteration
-        return {done: false, value: this.current++}
+        return { done: false, value: this.current++ };
       } else {
         //finish iteration
-        return {done: true}
+        return { done: true };
       }
-    }
-  }
+    },
+  };
   //return iterable object
-  return(iterObj)
+  return iterObj;
 }
