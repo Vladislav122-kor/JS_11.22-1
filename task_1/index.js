591d3ee0969e3fdc33ae1df69ec4bb9887097084
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-11-09 10:22:33 +0100

    [task_1]: task is done

---
+++ task_1/index.js
@@ -0,0 +1,24 @@
+// Use Object.defineProperty static method to set the method to the
+// Array.prototype
+// We can also set another property descriptors (e.g. writeble: false) to prevent unintentional changes of this method
+
+Object.defineProperty(Array.prototype, 'customFilter', {
+  // Implementation of filter method extended with thisArg property
+  value: function (callback, thisArg) {
+    if (
+      typeof callback !== 'function' ||
+      typeof thisArg !== 'object' ||
+      Array.isArray(thisArg) ||
+      thisArg == null
+    ) {
+      throw new Error()
+    }
+    const filteredArray = []
+    this.forEach((item, index, array) => {
+      if (callback.call(thisArg, item, index, array)) {
+        filteredArray.push(item)
+      }
+    })
+    return filteredArray
+  }
+})
