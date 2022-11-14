2fd5b8f20a424956a6040fd48b57e96fcfac4b4c
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-11-14 09:27:52 +0100

    [task_2]: task is done

--- task_2/index.js
+++ task_2/index.js
@@ -42,30 +42,3 @@ class Calculator {
       return this.arg1 / this.arg2
   }
 }
-
-const calculator = new Calculator(2, 2)
-console.log(calculator)
-console.log(calculator.getSum()); // 15
-console.log(calculator.getDiv()); // 4
-calculator.setX(15);
-console.log(calculator)
-const getCalculatorDiv = calculator.getDiv;
-const getCalculatorSum = calculator.getSum;
-const getCalculatorSub = calculator.getSub;
-const getCalculatorMul = calculator.getMul;
-console.log(getCalculatorDiv()) // 5
-console.log(getCalculatorSum()) 
-console.log(getCalculatorSub()) 
-console.log(getCalculatorMul()) 
-calculator.setX(21)
-console.log(getCalculatorDiv())
-const getCalcSetThis = calculator.setY
-getCalcSetThis(100)
-console.log(calculator)
-const getCalcSetThisX = calculator.setX
-getCalcSetThisX(10)
-console.log(calculator)
-console.log(getCalculatorDiv())
-const calculator2 = new Calculator(1111, 1111)
-console.log(calculator)
-console.log(calculator2)
