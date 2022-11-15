5d1fc635eb860a6347c2d7ffdbba1c3ee26d097f
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-11-15 14:22:45 +0100

    [task_2]: task is done

--- task_2/index.js
+++ task_2/index.js
@@ -1,3 +1,13 @@
+// It's interesting, binding objects in constructor
+// 'breaks' class inheritance
+// Now, the class methods not inhereting from class constructor, 
+// new methods are creating in every new instance instead! 
+// So, every instance will have its own setX, getMul, etc.
+// const calc = new Calculator(1,2)
+// Object.hasOwn(calc, 'setX')    //true
+// Object.hasOwn(calc, 'getSum')  //true
+// Thanks for such an interesting example :)
+
 class Calculator {
   constructor(arg1, arg2) {
     //binding this to new class that will be created
@@ -45,3 +55,34 @@ class Calculator {
       return this.arg1 / this.arg2
   }
 }
+// or we can directly declare all our methods in constructor 
+// with arrow functions. As for me, this syntax is more readeble.
+// But I will appreciate, if you'll comment what way is preferable
+
+// class Calculator {
+//   constructor(arg1, arg2) {
+//     this.arg1 = arg1
+//     this.arg2 = arg2
+//     this.setX = (num) => {
+//       if (!Number.isFinite(num)) {
+//         throw new Error()
+//       }
+//       this.arg1 = num
+//     }
+//     this.setY = (num) => {
+//       if (!Number.isFinite(num)) {
+//         throw new Error()
+//       }
+//       this.arg2 = num
+//     }
+//     this.getDiv = () => {
+//       if(this.arg2 === 0) {
+//         throw new Error()
+//       }
+//         return this.arg1 / this.arg2
+//     }
+//     this.getSum = () => this.arg1 + this.arg2
+//     this.getMul = () => this.arg1 * this.arg2
+//     this.getSub = () => this.arg1 - this.arg2
+//   }
+// }
